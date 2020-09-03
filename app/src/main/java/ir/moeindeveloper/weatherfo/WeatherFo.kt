19d01049adcf24f0d6cc3937cda42d523463b8b4
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2020-09-03 20:46:21 +0430

    init lingver lib

--- app/src/main/java/ir/moeindeveloper/weatherfo/WeatherFo.kt
+++ app/src/main/java/ir/moeindeveloper/weatherfo/WeatherFo.kt
@@ -1,8 +1,13 @@
 package ir.moeindeveloper.weatherfo
 
 import android.app.Application
+import com.yariksoffice.lingver.Lingver
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class WeatherFo: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Lingver.init(this,"en")
+    }
 }
